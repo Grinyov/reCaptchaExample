2fd66b4708e8d8fe8df57735fa899438b7fbabf6
green <vgrinyov@gmail.com>
2017-01-22 17:38:34 +0200

    add support angular js to form

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -3,5 +3,8 @@
  */
 angular.module("reCaptcha", [])
     .controller("AppCtrl", function ($scope) {
-
+        $scope.auth = {};
+        $scope.sendForm = function (auth) {
+            window.alert(auth.email);
+        }
     });
