e3621f799205c9cb9bc6be707aef689250d3545d
green <vgrinyov@gmail.com>
2017-03-14 22:48:54 +0200

    add functions onRecaptchaSubmit and onRecaptchaExpired to js

--- src/main/resources/public/js/main.js
+++ src/main/resources/public/js/main.js
@@ -35,6 +35,9 @@ angular.module("reCaptcha", [])
                     "expired-callback": onRecaptchaExpired
                 });
             };
+
+            window.onRecaptchaSubmit(){}
+            window.onRecaptchaExpired(){}
         }
     }
 
