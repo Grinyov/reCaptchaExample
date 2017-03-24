94d0f24b60c3beda20861f34576ad214da2b6555
green <vgrinyov@gmail.com>
2017-03-24 22:51:20 +0200

    add recapcha verify url

--- src/main/java/com/grinyov/recaptcha/service/ReCaptchaApiClient.java
+++ src/main/java/com/grinyov/recaptcha/service/ReCaptchaApiClient.java
@@ -2,14 +2,20 @@ package com.grinyov.recaptcha.service;
 
 import com.grinyov.recaptcha.dto.ReCaptchaResponseDto;
 import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by vgrinyov.
  */
 @Component
 public class ReCaptchaApiClient {
+    @Value("${app.reCaptcha.apiUrl}")
+    private String reCaptchaApiUrl;
+    private RestTemplate restTemplate = new RestTemplate();
+
     public ReCaptchaResponseDto verify(@NonNull String recaptchaResponseDto){
-        return null;
+        return restTemplate.postForObject(reCaptchaApiUrl);
     }
 }
